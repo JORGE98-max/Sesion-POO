7caa84a8bba01916e48514a141a47fb786579869
Your Name <you@example.com>
2020-06-25 15:23:23 -0500

    Updated

--- SESION POO/src/Principal.java
+++ SESION POO/src/Principal.java
@@ -2,7 +2,24 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		arbol tree = new arbol();
+		
+		tree.setCantidadHojas(800);
+		System.out.println(tree.getCantidadHojas());
+		
+		tree.setAltura(2.56);
+		System.out.println(tree.getAltura()+ " m ");
+		
+		tree.setAntiguedad(36);
+		System.out.println(tree.getAntiguedad() + " a\u00f1os");
+		
+		tree.setTipo("Roble Australiano");
+		System.out.println(tree.getTipo());
+		
+		tree.crearHojas();
+		tree.crecer();
+		tree.fotosintesis();
 
 	}
 
--- SESION POO/src/arbol.java
+++ SESION POO/src/arbol.java
@@ -1,9 +1,64 @@
 
 public class arbol {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// ATRIBUTOS
+	
+	private int cantidadHojas;
 
+	
+
+	public int getCantidadHojas() {
+		return cantidadHojas;
 	}
 
+	public void setCantidadHojas(int cantidadHojas) {
+		this.cantidadHojas = cantidadHojas;
+	}
+
+	private String Tipo;
+
+	public String getTipo() {
+		return Tipo;
+	}
+
+	public void setTipo(String tipo) {
+		Tipo = tipo;
+	}
+
+	private int Antiguedad;
+
+	public int getAntiguedad() {
+		return Antiguedad;
+	}
+
+	public void setAntiguedad(int antiguedad) {
+		Antiguedad = antiguedad;
+	}
+
+	private double altura;
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	//METODOS
+	
+	void crecer() {
+
+		System.out.println("Crece");
+	}
+
+	void crearHojas() {
+
+		System.out.println("Crea hojas");
+	}
+
+	void fotosintesis() {
+
+		System.out.println("fotosintesis");
+	}
 }
